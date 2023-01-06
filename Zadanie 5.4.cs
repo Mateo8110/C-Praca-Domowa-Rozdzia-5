338b27b594c7daf94e9f79c6adcbd257d8e7df4a
Mateo8110 <94134843+Mateo8110@users.noreply.github.com>
2023-01-06 12:50:04 +0100

    Add files via upload

---
+++ Zadanie 5.4.cs
@@ -0,0 +1,28 @@
+﻿namespace Zadanie
+{
+    class Program
+    {
+        static int[] Mnozenie( int mnoznik1, int[] tab)
+        {
+            int[] tab1 = { 1, 4, 6, 8, 2 };
+
+            for (int i = 0; i < tab1.Length; i++)
+            {
+                tab1[i] = tab1[i] * mnoznik1;
+                Console.Write(tab1[i] + "  ");
+            }
+            return tab1;
+        }
+        static void Main(string[] args)
+        {
+            int mnoznik1;
+            int[] tab2 = new int[5];
+            Console.WriteLine("Podaj mnożnik: ");
+            mnoznik1 = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            int[] tab = Mnozenie(mnoznik1, tab2);
+
+            Console.ReadKey();
+        }
+    }
+}
