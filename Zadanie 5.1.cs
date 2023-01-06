78462e3141bd0c109b874ef5e0ee3466040efa7e
Mateo8110 <94134843+Mateo8110@users.noreply.github.com>
2023-01-06 12:48:41 +0100

    Add files via upload

---
+++ Zadanie 5.1.cs
@@ -0,0 +1,19 @@
+﻿namespace Program
+{
+    class Zadanie
+    {
+        static void Celsjusz(double f)
+        {
+            Console.WriteLine("Temperatura wynosi: " + (f - 32) / (1.8));
+        }
+        static void Main(string[] args)
+        {
+            double x;
+            Console.WriteLine("Podaj temperarture w stopniach Fahrenheita");
+            x = double.Parse(Console.ReadLine());
+            Celsjusz(x);
+            Console.ReadKey();
+        }
+    }
+
+}
