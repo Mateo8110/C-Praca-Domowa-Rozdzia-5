967365b04f4808430d5e141b079a65af0d4b8756
Mateo8110 <94134843+Mateo8110@users.noreply.github.com>
2023-01-06 12:55:50 +0100

    Add files via upload

---
+++ Zadanie 5.9b.cs
@@ -0,0 +1,26 @@
+﻿namespace Program
+{
+    class Zadanie
+    {
+        static int ciagFib(int n)
+        {
+
+            while (n == 1 || n == 2)
+            {
+                return 1;
+            }
+            return ciagFib(n - 1) + ciagFib(n - 2);
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            int n = 0;
+            Console.WriteLine("Podaj n-ty wyraz ciągu Fibonnaciego: ");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine(n + "-ty wyraz ciągu fibonnaicego to: " + ciagFib(n));
+            Console.ReadKey();
+        }
+    }
+}
