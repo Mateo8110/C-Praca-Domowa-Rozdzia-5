e9b2f4ad1609001b53036ca40f5db9f2f16a7e50
Mateo8110 <94134843+Mateo8110@users.noreply.github.com>
2023-01-06 12:56:24 +0100

    Add files via upload

---
+++ Zadanie5.10.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Zadanie
+{
+    class Program
+    {
+        static int Oblicz(int n)
+        {
+            if (n <= 1)
+                return 1;
+            else
+                return (n + Oblicz(n - 1));
+        }
+
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Podaj n: ");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine(Oblicz(n));
+            Console.ReadKey();
+        }
+    }
+}
