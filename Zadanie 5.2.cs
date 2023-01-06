f56a44c0ea9309e400418f2c36b693417ac36153
Mateo8110 <94134843+Mateo8110@users.noreply.github.com>
2023-01-06 12:49:11 +0100

    Add files via upload

---
+++ Zadanie 5.2.cs
@@ -0,0 +1,35 @@
+﻿namespace Zadanie
+{
+    class Program
+    {
+        static int Przedzial(int a, int b, int x)
+        {
+            if (a < x && x < b) return 1;
+            else return 0;
+        }
+        static void Main(string[] args)
+        {
+            int a, b, x;
+
+            Console.WriteLine("Podaj a: ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj b: ");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj x: ");
+            x = int.Parse(Console.ReadLine());
+
+            
+
+            if (Przedzial(a, b, x) == 1)
+                Console.WriteLine("x należy do przedziału obustronnie otwartego");
+            else
+                Console.WriteLine("X nie należey do przedziału obustronnie otwartego");
+
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
