48aa7784ee309eca0ca70c27cea0e7d5b58eb3ae
Mateo8110 <94134843+Mateo8110@users.noreply.github.com>
2023-01-06 12:53:36 +0100

    Add files via upload

---
+++ Zadanie 5.5.cs
@@ -0,0 +1,41 @@
+﻿namespace Zadanie
+{
+    class Program
+    {
+        
+        static void Rysuj(int szerokosc, int dlugosc, string znak)
+        {
+            for (int i = 0; i < dlugosc; i++)
+            {
+                for (int j = 0; j < szerokosc; j++)
+                {
+                    Console.Write(znak);
+                }
+                Console.WriteLine();
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            int dlugosc, szerokosc;
+            string znak;
+
+            Console.WriteLine("Podaj rozmiary prostokąta i znak ");
+            Console.WriteLine();
+            Console.WriteLine("Podaj dlugosc: ");
+            dlugosc = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj szerokosc: ");
+            szerokosc = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj znak: ");
+            znak = Console.ReadLine();
+
+
+            Rysuj(dlugosc, szerokosc, znak);
+            
+            
+            
+
+            Console.ReadKey();
+        }
+    }
+}
