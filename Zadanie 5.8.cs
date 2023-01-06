353182d0d26506e0ebd27eed9e860ae42ef79e26
Mateo8110 <94134843+Mateo8110@users.noreply.github.com>
2023-01-06 12:54:58 +0100

    Add files via upload

---
+++ Zadanie 5.8.cs
@@ -0,0 +1,31 @@
+﻿namespace Zadanie
+{
+    class Program
+    {
+
+        static int SumaCyfr(int x, int wynik)
+        {
+            while(x != 0)
+            {
+                wynik = wynik + (x % 10);
+                x = x / 10;
+            }
+            return wynik;
+        }
+        
+        
+        
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int wynik1 = 0;  
+            Console.WriteLine("Podaj x: ");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Suma cyfr podanej liczby wynosi: {0}", SumaCyfr(x, wynik1));
+            
+            Console.ReadKey();
+
+        }
+    }
+}
