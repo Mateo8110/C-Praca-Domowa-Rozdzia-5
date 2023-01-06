674fa64d16878ccfaba8543669a4eda56a4b5b9c
Mateo8110 <94134843+Mateo8110@users.noreply.github.com>
2023-01-06 12:54:30 +0100

    Add files via upload

---
+++ Zadanie 5.7.cs
@@ -0,0 +1,24 @@
+﻿namespace Zadanie
+{
+    class Program
+    {
+        static void Ciag(int x,int n, int suma)
+        {
+            Console.WriteLine(suma = (2 * x + 1 + n) * n / 2);
+        }
+           
+        static void Main(string[] args)
+        {
+            int x, n;
+            int suma = 0;
+            Console.WriteLine("Podaj x: ");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj n: ");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("N-ty wyraz ciągu wynosi: ");
+            Ciag(x,n,suma);
+
+            Console.ReadKey();
+        }
+    }
+}
